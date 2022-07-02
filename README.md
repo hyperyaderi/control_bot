4193c48b007049afb56fb7467c8ea0e0cfa03185
Maxim <me@clownless.one>
2022-07-02 19:32:49 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# control_bot
+
+# Запуск
+1. Установите необходимые библиотеки
+```bash 
+pip install -r requirements.txt
+```
+2. Переименуйте файл `.env.example` в `.env` и подставьте туда свои данные
+3. Запустите бота
+```bash
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import logging
+import aiogram
+
+from dotenv import load_dotenv
+from aiogram.utils import executor
+from aiogram import Bot, types, filters
+from aiogram.dispatcher import Dispatcher
+load_dotenv('.env')
+
+bot = Bot(token=os.environ.get('TG_TOKEN'))
+dp = Dispatcher(bot)
+whitelist = [1888296065, 1999113390, 1618915689, 834381991]
+
+@dp.message_handler(filters.Command('/stopices'))
+async def stopices(message: types.Message):
+    if message.from_user.id not in whitelist:
+        await message.reply('Пошёл нахуй!')
+    else:
+        os.system('/home/icecast/stopices')
+        await message.reply('Ices стопнут, можете врываться с радиобосса')
+
+@dp.message_handler(filters.Command('/startices'))
+async def startices(message: types.Message):
+    if message.from_user.id not in whitelist:
+        await message.reply('Пошёл нахуй!')
+    else:
+        os.system('/home/icecast/startices')
+        await message.reply('Ices запущен, ведется поток плейлиста')
